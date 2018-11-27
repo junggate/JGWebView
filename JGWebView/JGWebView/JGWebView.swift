d34183672f58a10f91ecbf92eb841b3c6a2a212c
maingate <josansnim@gmail.com>
2018-11-27 10:24:58 +0900

    기본 프로젝트 추가

---
+++ JGWebView/JGWebView/JGWebView.swift
@@ -0,0 +1,14 @@
+//
+//  JGWebView.swift
+//  JGWebView
+//
+//  Created by JungMoon-Mac on 27/11/2018.
+//  Copyright © 2018 JungMoon. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class JGWebView: WKWebView {
+
+}
---
+++ JGWebViewExample/JGWebViewExample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  JGWebViewExample
+//
+//  Created by JungMoon-Mac on 27/11/2018.
+//  Copyright © 2018 JungMoon. All rights reserved.
+//
+
+import UIKit
+import JGWebView
+class ViewController: UIViewController {
+
+    @IBOutlet weak var jgWebView: JGWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
